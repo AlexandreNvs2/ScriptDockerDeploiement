bcaa7e9f70e772108ff5be117c49fef8b436c8c5
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2025-07-12 20:17:43 +0200

    ScriptDocker init commit

---
+++ DockerDeploiement.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##################################################################
+#
+#	Description -> Script pour déploiement de conteneur Docker
+#
+#	
+#
+##################################################################
+
+
+echo"
+
+Options : 
+
+	- --create : lancer des conteneur
+	- --drop : Supprimer des conteneur créer par le script 
+	- --infos : Caractéristiques des conteneur
+	- --start : Démarage des conteneur
+	- --ansible : Deploiement ansible
+	
+"

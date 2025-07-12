34a061b6b3594b5ff3f3b160615bf2ea2ceeee96
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2025-07-12 21:49:54 +0200

    [DEV] -> Conditions pour les options du Script

--- DockerDeploiement.sh
+++ DockerDeploiement.sh
@@ -8,6 +8,35 @@
 #
 ##################################################################
 
+#si option --create 
+if [ "$1" == "--create" ];then 
+
+	echo "L'option choisi est --create"
+
+
+
+#Si option --drop
+elif [ "$1" == "--drop" ];then
+
+	echo "L'option choisi est --drop"
+
+
+#Si option --start
+elif [ "$1" == "--start" ];then
+
+	echo "L'option choisi est --start"
+
+
+
+#si option --ansible
+elif [ "$1" == "--ansible" ]; then
+
+	echo "L'option choisi est --ansible"
+
+
+
+# si aucune option affiche de l'aide
+else
 
 echo"
 
@@ -20,3 +49,4 @@ Options :
 	- --ansible : Deploiement ansible
 	
 "
+fi

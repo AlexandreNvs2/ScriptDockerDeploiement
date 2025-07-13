653ec6eb6eb01575891f4822caedc91e8f7e5f02
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2025-07-13 16:13:46 +0200

    [DEV] -> Mise en place de la variable idmax

--- DockerDeploiement.sh
+++ DockerDeploiement.sh
@@ -11,12 +11,17 @@
 #Si option --create 
 if [ "$1" == "--create" ];then 
 
+
+
+
 	echo "L'option choisi est --create"
 
 #Définition grâce à $2 du nombre de conteneur voulu
 	nb_machine=1
 	 [ "$2" != "" ] && nb_machine=$2
 
+#On récupère l'id max 
+idmax='docker ps -a --format '{{ .Names }}' | awk -F "-" -v user=$USER '$0 ~ user"-alpine" {print $3} | sort -r | head -1'
 
 #Boucle for pour créer les conteneur en fonction du $2
 echo"Le(s) Conteneurs sont en cours de deploiement... "
